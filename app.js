cbdbfe0ed29148a3661284f683ee23ac9d5b623e
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-22 11:59:25 -0500

    deleted debug code

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+// const app_name = require('./package.json').name;
+// const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 // Layout Middleware
 app.use((req, res, next) => {
